5881c22548b07aa41d569da2c4f46d5e8cf8a29b
Mario Molinero <mariomolinero78@gmail.com>
2025-01-09 22:03:44 -0600

    Agregar archivos de proyecto.

---
+++ AseguradoraRiver/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using AseguradoraRiver.ViewModels;
+using Xamarin.Forms;
+
+namespace AseguradoraRiver.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AseguradoraRiver/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using AseguradoraRiver.Models;
+using AseguradoraRiver.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AseguradoraRiver.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
